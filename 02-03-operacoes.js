239be8d90430ca56157978a4fbf5b07b63673d14
GEANANGELO <geanangelodasilva@hotmail.com>
2022-09-27 01:06:05 -0300

    add 02-03-operacoes.js ao repositorio

---
+++ 02-03-operacoes.js
@@ -0,0 +1,22 @@
+//função de media
+function media(num1, num2){
+    let resultado = (num1 + num2)/2; 
+    return resultado;    
+}
+//aqui chamamos a função e lhe passamos
+//os parametros (2,3)
+//media(2,3);
+console.log('a media é ' + media(2,3));
+
+//função de media
+function media(num1, num2){
+    let restodivisao = num1 % num2; 
+    return restodivisao;    
+}
+//aqui chamamos a função e lhe passamos
+//os parametros (4,2)
+//media(4,2);
+console.log('o resto da divisao é ' + media(3,2));
+
+
+
