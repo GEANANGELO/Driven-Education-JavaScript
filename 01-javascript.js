fbd20f7824dbe9136356805e8cb52775a41519c6
GEANANGELO <geanangelodasilva@hotmail.com>
2022-09-27 01:04:53 -0300

    add 01-javascript.js ao repositorio

---
+++ 01-javascript.js
@@ -0,0 +1,9 @@
+function somar(num1, num2){
+    let soma = num1 + num2; 
+    return soma;    
+}
+//aqui chamamos a função e lhe passamos
+//os parametros (2,3)
+//somar(2,3);
+console.log(somar(2,3));
+
